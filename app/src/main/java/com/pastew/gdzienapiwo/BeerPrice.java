f1e547b8aa9b40e358b5dc92f6f19ab70e2e188c
Pastew <kpastew@gmail.com>
2015-10-24 01:33:20 +0200

    Pub and BeerPrice schema clasees

---
+++ app/src/main/java/com/pastew/gdzienapiwo/BeerPrice.java
@@ -0,0 +1,12 @@
+package com.pastew.gdzienapiwo;
+
+import java.util.Currency;
+
+/**
+ * Created by Pastew on 2015-10-24.
+ */
+public class BeerPrice {
+
+    public Currency price;
+    public int votes;
+}
---
+++ app/src/main/java/com/pastew/gdzienapiwo/Pub.java
@@ -0,0 +1,16 @@
+package com.pastew.gdzienapiwo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Pastew on 2015-10-24.
+ */
+public class Pub {
+    private int id;
+    private ArrayList<BeerPrice> beerPrices;
+    private String address;
+
+    public Pub(){
+
+    }
+}
